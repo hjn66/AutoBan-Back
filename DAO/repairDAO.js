6c0d0b31d798feda89c0b14cef0c726387e5cc3f
autoban66 <autoban.hjn66@gmail.com>
2019-01-11 19:36:13 +0330

    Create removeRepair Function

--- DAO/repairDAO.js
+++ DAO/repairDAO.js
@@ -7,3 +7,7 @@ const Op = Sequelize.Op;
 module.exports.addRepair = async function(title, date, totalCost, garageName, garageId, creatorId, carId) {
   return await Repair.create({ title, date, totalCost, garageName, garageId, creatorId, carId });
 };
+
+module.exports.removeRepair = async function(repair) {
+  return await repair.destroy();
+};
