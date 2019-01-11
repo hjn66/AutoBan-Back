47262e968753076b06fbd256d43c8f9524439c2e
autoban66 <autoban.hjn66@gmail.com>
2019-01-11 19:30:43 +0330

    Create addRepair Functon

---
+++ DAO/repairDAO.js
@@ -0,0 +1,9 @@
+const Repair = require("../startup/sequelize").Repair;
+const bcrypt = require("bcryptjs");
+const Utils = require("../middlewares/utils");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
+module.exports.addRepair = async function(title, date, totalCost, garageName, garageId, creatorId, carId) {
+  return await Repair.create({ title, date, totalCost, garageName, garageId, creatorId, carId });
+};
