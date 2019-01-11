d2880ea15d6cd45e8ffb511c601f6716742cb4a2
autoban66 <autoban.hjn66@gmail.com>
2019-01-11 19:24:11 +0330

    Add getGarageById Function

--- DAO/garageDAO.js
+++ DAO/garageDAO.js
@@ -6,3 +6,11 @@ const Op = Sequelize.Op;
 module.exports.addGarage = async function(name, phoneNumber, latitude, longitude, image, address) {
   return await Garage.create({ name, phoneNumber, latitude, longitude, image, address });
 };
+
+module.exports.getGarageById = async function(id) {
+  var garage = await Garage.findByPk(id);
+  if (!garage) {
+    throw new Error("Garage not found");
+  }
+  return garage;
+};
