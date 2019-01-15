3e15288269e6003273a3403a9bc67ff117a6357b
hjn66 <h.niloofar@gmail.com>
2019-01-16 01:15:31 +0330

    Add logs & uploads directory to mkdir

--- startup/mkdir.js
+++ startup/mkdir.js
@@ -3,6 +3,8 @@ const path = require("path");
 const config = require("config");
 
 module.exports = function() {
+  mkdirIfNotExist(path.join(rootPath, config.get("logs_dir")));
+  mkdirIfNotExist(path.join(rootPath, config.get("uploads_dir")));
   mkdirIfNotExist(path.join(rootPath, config.get("user_images_dir")));
   mkdirIfNotExist(path.join(rootPath, config.get("car_images_dir")));
   mkdirIfNotExist(path.join(rootPath, config.get("receipt_images_dir")));
