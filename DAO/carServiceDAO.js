550d6523097edd11285fc000b738665c498f0cfb
hjn66 <h.niloofar@gmail.com>
2019-01-18 02:43:41 +0330

    Add updateCarService() & getServiceById()

--- DAO/carServiceDAO.js
+++ DAO/carServiceDAO.js
@@ -1,14 +1,26 @@
-const CarService = require('../startup/sequelize').CarService;
-const bcrypt = require('bcryptjs');
-const Utils = require('../middlewares/utils');
-const Sequelize = require('sequelize');
+const CarService = require("../startup/sequelize").CarService;
+const bcrypt = require("bcryptjs");
+const Utils = require("../middlewares/utils");
+const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
+
+module.exports.getServiceById = async function(id) {
+  service = await CarService.findByPk(id);
+  if (!service) {
+    throw new Error("Car Service not found");
+  }
+  return service;
+};
 
 module.exports.addCarService = async function(persianName, englishName) {
   return await CarService.create({
     persianName: persianName,
     englishName: englishName
   });
+};
+
+module.exports.updateCarService = async function(CarService) {
+  return await CarService.save();
 };
 
 module.exports.listCarService = async function() {
