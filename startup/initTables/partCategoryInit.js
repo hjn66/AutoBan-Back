c49efa919f194e9a78cbf96b7f572ccea68eeab7
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-01-24 18:45:03 +0330

    Add partCategoryInit

---
+++ startup/initTables/partCategoryInit.js
@@ -0,0 +1,41 @@
+module.exports = async PartCategory => {
+  await PartCategory.create({
+    persianName: "روغن موتور",
+    englishName: "Engine Oil",
+    checkPeriodic: true
+  });
+  await PartCategory.create({
+    persianName: "فیلتر روغن",
+    englishName: "Oil Filter",
+    checkPeriodic: true
+  });
+  await PartCategory.create({
+    persianName: "فیلتر هوا",
+    englishName: "Air Filter",
+    checkPeriodic: true
+  });
+  await PartCategory.create({
+    persianName: "فیلتر کابین",
+    englishName: "Cab Filter",
+    checkPeriodic: true
+  });
+  await PartCategory.create({
+    persianName: "فیلتر بنزین",
+    englishName: "Gasoline Filter",
+    checkPeriodic: true
+  });
+  await PartCategory.create({
+    persianName: "روغن هیدرولیک",
+    englishName: "Hydraulic Oil",
+    checkPeriodic: true
+  });
+  await PartCategory.create({
+    persianName: "روغن گیربکس",
+    englishName: "Gearbox Oil",
+    checkPeriodic: true
+  });
+  await PartCategory.create({
+    persianName: "ضد یخ",
+    englishName: "Antifreeze"
+  });
+};
