cac94366093ba544ec348f94d67bbcf9b1201131
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-01-25 02:18:02 +0330

    Add getColorByName() & getColorById() and Translations

--- DAO/colorDAO.js
+++ DAO/colorDAO.js
@@ -1,5 +1,21 @@
 const Color = require("../startup/sequelize").Color;
 
+module.exports.getColorById = async function(id) {
+  color = await Color.findByPk(id);
+  if (!color) {
+    throw new Error("Color not found");
+  }
+  return color;
+};
+
+module.exports.getColorByName = async function(persianName) {
+  color = await Color.findOne({ where: { persianName } });
+  if (!color) {
+    throw new Error("Color not found");
+  }
+  return color;
+};
+
 module.exports.listColors = async function() {
   return await Color.findAll();
 };
