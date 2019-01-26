bdb014a60639cf714940e778afe862da55247272
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-01-26 15:52:52 +0330

    Change Static Folders

--- startup/routes.js
+++ startup/routes.js
@@ -1,23 +1,23 @@
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const express = require("express");
-const path = require("path");
-const passport = require("passport");
-require("express-async-errors");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const path = require('path');
+const passport = require('passport');
+require('express-async-errors');
 
-const users = require("../routes/users");
-const cars = require("../routes/cars");
-const costs = require("../routes/costs");
-const repairs = require("../routes/repairs");
-const periodcServices = require("../routes/periodcServices");
+const users = require('../routes/users');
+const cars = require('../routes/cars');
+const costs = require('../routes/costs');
+const repairs = require('../routes/repairs');
+const periodcServices = require('../routes/periodcServices');
 
 module.exports = async function(app) {
   // CORS Middleware
   app.use(cors());
 
   // Set Static Folder
-  app.use(express.static(path.join(__dirname, "/../public")));
-  app.use(express.static(path.join(__dirname, "/../uploads")));
+  app.use(express.static(path.join(rootPath, 'public')));
+  app.use('/uploads', express.static(path.join(rootPath, 'uploads')));
 
   // Body Parser Middleware
   app.use(bodyParser.json());
@@ -26,11 +26,11 @@ module.exports = async function(app) {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  require("../middlewares/passport")(passport);
+  require('../middlewares/passport')(passport);
 
-  app.use("/users", users);
-  app.use("/cars", cars);
-  app.use("/costs", costs);
-  app.use("/repairs", repairs);
-  app.use("/periodic-services", periodcServices);
+  app.use('/users', users);
+  app.use('/cars', cars);
+  app.use('/costs', costs);
+  app.use('/repairs', repairs);
+  app.use('/periodic-services', periodcServices);
 };
