4194cc45dc0619dddb54b29b32fcdf07469a0893
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-02-04 01:53:27 +0330

    Create perioodicServiceDAO

---
+++ DAO/periodicServiceDAO.js
@@ -0,0 +1,16 @@
+const config = require("config");
+const PeriodicService = require("../startup/sequelize").PeriodicService;
+const Cost = require("../startup/sequelize").Cost;
+
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
+module.exports.addPeriodicServiceItems = async function(
+  repairId,
+  serviceItems
+) {
+  for (const index in serviceItems) {
+    serviceItems[index].repairId = repairId;
+    await PeriodicService.create(serviceItems[index]);
+  }
+};
