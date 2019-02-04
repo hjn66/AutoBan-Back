6446d7604eaf4e93cca29f7161725441e676a8d7
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-02-04 19:37:11 +0330

    Change categoryId to id

--- DAO/periodicServiceDAO.js
+++ DAO/periodicServiceDAO.js
@@ -1,8 +1,8 @@
-const config = require("config");
-const PeriodicService = require("../startup/sequelize").PeriodicService;
-const Cost = require("../startup/sequelize").Cost;
+const config = require('config');
+const PeriodicService = require('../startup/sequelize').PeriodicService;
+const Cost = require('../startup/sequelize').Cost;
 
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 module.exports.addPeriodicServiceItems = async function(
@@ -11,6 +11,7 @@ module.exports.addPeriodicServiceItems = async function(
 ) {
   for (const index in serviceItems) {
     serviceItems[index].repairId = repairId;
+    serviceItems[index].categoryId = id;
     await PeriodicService.create(serviceItems[index]);
   }
 };
