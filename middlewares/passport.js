160775eb3f7417fc51afb950ebc628d1729985c5
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-02-08 23:07:31 +0330

    change require to rootRequire

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,15 +1,15 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const config = require('config');
-const UserDAO = require('../DAO/userDAO');
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const config = require("config");
+const UserDAO = rootRequire("DAO/userDAO");
 
 module.exports = function(passport) {
   let opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
-  opts.secretOrKey = config.get('JWTsecret');
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+  opts.secretOrKey = config.get("JWTsecret");
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
-      if (jwt_payload.type == 'SMS') {
+      if (jwt_payload.type == "SMS") {
         return done(null, jwt_payload.mobileNumber);
       }
       UserDAO.getById(jwt_payload.user.id, (err, user) => {
