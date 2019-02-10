665f559248370017742039fd4943d5c2533ff460
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-02-10 04:09:20 +0330

    change addMinutes Signature

--- DAO/smsTokenDAO.js
+++ DAO/smsTokenDAO.js
@@ -21,14 +21,25 @@ module.exports.getToken = async function(mobileNumber) {
 // generate random Token and save on DB
 module.exports.createToken = async function(mobileNumber) {
   token = randToken.generate(config.get("sms_token_length"), "0123456789");
-  expired = await Utils.addminutes(new Date(), config.get("sms_token_expiration_min"));
-  return await SMSToken.create({ mobileNumber: mobileNumber, token: token, expiredDate: expired });
+  expired = await Utils.addMinute(
+    new Date(),
+    config.get("sms_token_expiration_min")
+  );
+  return await SMSToken.create({
+    mobileNumber: mobileNumber,
+    token: token,
+    expiredDate: expired
+  });
 };
 
 // generate random Token and save on DB
 module.exports.checkToken = async function(token) {
   token.mobileNumber = await Utils.internationalMobile(token.mobileNumber);
-  smsToken = await SMSToken.findOne({ where: { [Op.and]: [{ mobileNumber: token.mobileNumber }, { token: token.token }] } });
+  smsToken = await SMSToken.findOne({
+    where: {
+      [Op.and]: [{ mobileNumber: token.mobileNumber }, { token: token.token }]
+    }
+  });
   if (smsToken) {
     if (smsToken.expiredDate < Date.now()) {
       throw new Error("Expired token");
