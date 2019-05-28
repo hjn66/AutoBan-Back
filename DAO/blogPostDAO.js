f91c19fd24a8aaabe271af9b4dffb840588f729d
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-05-28 23:36:48 +0430

    Remove Unnsseccery Includes

--- DAO/blogPostDAO.js
+++ DAO/blogPostDAO.js
@@ -1,9 +1,6 @@
 const config = require("config");
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
 
 const BlogPost = rootRequire("startup/sequelize").BlogPost;
-const Utils = rootRequire("middlewares/utils");
 
 module.exports.getById = async function(id) {
   post = await BlogPost.findByPk(id);
