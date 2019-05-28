25c5bb31f90593d2179a98fffd43b8d4d0b7f5e6
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-05-28 23:52:52 +0430

    Add unlike()

--- DAO/blogLikeDAO.js
+++ DAO/blogLikeDAO.js
@@ -12,3 +12,13 @@ module.exports.like = async function(userId, blogPostId) {
     blogId: blogPostId
   });
 };
+
+module.exports.unlike = async function(userId, blogPostId) {
+  let like = await BlogLike.findOne({
+    where: { userId, blogId: blogPostId }
+  });
+  if (!like) {
+    throw new Error("You must like a post before unlike");
+  }
+  return await like.destroy();
+};
